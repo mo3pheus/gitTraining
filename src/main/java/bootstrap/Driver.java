149acf77ce6d15ade3a6db9e98f2d793d82b70e7
Sanket <sanket.korgaonkar@gmail.com>
2020-07-23 20:08:52 -0400

    Feature: Added feature to compute area of square.

--- src/main/java/bootstrap/Driver.java
+++ src/main/java/bootstrap/Driver.java
@@ -1,5 +1,8 @@
 package bootstrap;
 
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Driver {
     public static final String SEPARATOR =
             "==============================================================";
@@ -7,6 +10,17 @@ public class Driver {
     public static void main(String[] args) {
         System.out.println(SEPARATOR);
         System.out.println("Hello and welcome to git training");
+        processSquareArea();
         System.out.println(SEPARATOR);
+    }
+
+    static void processSquareArea() {
+        System.out.println("This utility helps you calculate the area of a square.");
+        System.out.println("Please enter length of side::");
+        int     side    = 0;
+        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+        side = scanner.nextInt();
+        long area = side * side;
+        System.out.println("The area of the square for side = " + side + " is " + area);
     }
 }
