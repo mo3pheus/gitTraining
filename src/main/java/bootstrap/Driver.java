a2ef2f5106629f6deb304ca2b01a04b414451fef
Anthony Thomas <anthonythomas40000@gmail.com>
2020-07-31 19:32:08 +0530

    Feature: wrote function for area of circle

--- src/main/java/bootstrap/Driver.java
+++ src/main/java/bootstrap/Driver.java
@@ -4,9 +4,22 @@ public class Driver {
     public static final String SEPARATOR =
             "==============================================================";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+    	int r;
+    	double area;
         System.out.println(SEPARATOR);
         System.out.println("Hello and welcome to git training");
         System.out.println(SEPARATOR);
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter radius");
     }
+}
+
+
+public static double getAreaCircle(int r)
+{
+	r=s.nextInt();
+	area=3.14d*r*r;
+	System.out.println("Area is:" + area);
 }
