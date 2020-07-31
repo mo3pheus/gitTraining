49093580d2badabb30b4769cfee1e759ef0cdcba
Cyril <sahayacyril2015@gmail.com>
2020-07-31 19:33:38 +0530

    Feature: Wrote function for area of triangle

--- src/main/java/bootstrap/Driver.java
+++ src/main/java/bootstrap/Driver.java
@@ -7,6 +7,13 @@ public class Driver {
     public static void main(String[] args) {
         System.out.println(SEPARATOR);
         System.out.println("Hello and welcome to git training");
+        int base = 2;
+        int height = 4;
+        System.out.println("Area of triangle with base and height = " + base + " " + height + " = " + getArea(base,height));
         System.out.println(SEPARATOR);
+    }
+
+    public static double getAreaTriangle(int base, int height){
+    	return 0.5d * base * height;
     }
 }
