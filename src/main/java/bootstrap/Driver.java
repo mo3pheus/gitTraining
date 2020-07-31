552b9e1a2cfa9c5114618edc9e79ac02ae4ec33d
Sanket <sanket.korgaonkar@gmail.com>
2020-07-31 10:01:23 -0400

    Feature: wrote function for area of square.

--- src/main/java/bootstrap/Driver.java
+++ src/main/java/bootstrap/Driver.java
@@ -7,6 +7,13 @@ public class Driver {
     public static void main(String[] args) {
         System.out.println(SEPARATOR);
         System.out.println("Hello and welcome to git training");
+        int side = 2;
+        System.out.println(" Area of square with side = " + side + " = " + getArea(side));
         System.out.println(SEPARATOR);
+    }
+
+    public static double getAreaSquare(int side){
+        // Sanket is calculating area of rectangle
+        return 3.14d * side * side;
     }
 }
